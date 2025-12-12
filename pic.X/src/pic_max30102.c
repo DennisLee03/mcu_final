e6f5e160b902b78343acfbc796747dc221a439fa
Dennis <dlee92928@gmail.com>
2025-12-12 10:49:50 +0800

    add some max30102's dev and reg info

--- pic.X/src/pic_max30102.c
+++ pic.X/src/pic_max30102.c
@@ -1,2 +1,11 @@
 #include <xc.h>
 #include "i2c_master.h"
+
+#define DEV_ADDR        0x57 // @todo we probably not use, delete it?
+#define DEV_WRITE       0xAE
+#define DEV_READ        0xAF
+#define REG_FIFO_WR_PTR 0x04
+#define REG_OVF_COUNTER 0x05
+#define REG_FIFO_RD_PTR 0x06
+#define REG_FIFO_DATA   0x07
+#define REG_MODE        0x09
